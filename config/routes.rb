6fa5e9737b564cfe397d3bea9d77138bd32f3eac
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2024-01-22 01:32:14 +0000

    <change>ルーティングをリソースに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'lists/top' => 'homes#top'
-  get 'lists' => 'lists#index'
-  get 'lists/new'
+
+  resources :lists
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
