c4f4f403e7f52fe1b748157b507cbe62ff53372f
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2024-01-22 01:33:52 +0000

    <add>クリエイトとショウアクション追記

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,28 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
   end
 
   def new
     @list = List.new
   end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to lists_path
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+
 end
